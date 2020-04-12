9463c458f4c69f933ef7901a13c42a2a34542095
Artem <thefirstbornson@gmail.com>
2020-04-12 13:16:48 +0200

    Initial commit

---
+++ src/main/java/ru/otus/highloadarch/HighloadArchApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.highloadarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighloadArchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighloadArchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/otus/highloadarch/HighloadArchApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.highloadarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighloadArchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

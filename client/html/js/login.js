17edbdd5ef9271622dd618924747988ddd298872
Artem <thefirstbornson@gmail.com>
2020-05-03 11:14:17 +0200

    docker final

---
+++ client/html/js/login.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function ($) {
+	$('#loginform').submit(function (event) {
+		event.preventDefault();
+
+		showMeYourCookies('At loginform submission');
+
+		var cookie = JSON.parse($.cookie('helloween'));
+		var data = 'username=' + $('#username').val() + '&password=' + $('#password').val();
+		$.ajax({
+			data: data,
+			// headers: {'X-CSRF-TOKEN': cookie.csrf},
+			// timeout: 1000,
+			type: 'POST',
+			url: '/api/login'
+
+		}).done(function(data, textStatus, jqXHR) {
+			showMeYourJqXHR('When loginform is done', jqXHR);
+			showMeYourCookies('When loginform is done');
+
+			// window.location = cookie.url;
+			window.location.href = '/index.html';
+		}).fail(function(jqXHR, textStatus, errorThrown) {
+			showMeYourJqXHR('When loginform fails', jqXHR);
+			showMeYourCookies('When loginform fails');
+
+			console.error('Booh! Wrong credentials, try again!');
+		});
+	});
+});

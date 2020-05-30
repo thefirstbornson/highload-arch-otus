520aabced1fe9d7dfa55c902f2528a0231b84518
Artem <thefirstbornson@gmail.com>
2020-05-30 14:16:01 +0200

    before 2 datasources - 1 datasource work

---
+++ src/main/java/ru/otus/highloadarch/config/DataSourceConfigWrite.java
@@ -0,0 +1,43 @@
+package ru.otus.highloadarch.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJdbcRepositories(
+        jdbcOperationsRef = "writeJdbcOperations",
+        basePackages = {"ru.otus.highloadarch.repository"}
+)
+public class DataSourceConfigWrite extends AbstractJdbcConfiguration {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource-write")
+    public DataSource writeDataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    NamedParameterJdbcOperations writeJdbcOperations(@Qualifier("writeDataSource") DataSource sqlServerDs) {
+        return new NamedParameterJdbcTemplate(sqlServerDs);
+    }
+
+}
